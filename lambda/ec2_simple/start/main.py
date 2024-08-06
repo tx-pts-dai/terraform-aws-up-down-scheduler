a0ba2865d625bd824b3ce376474786095e5f1d69
Davidoutz <david.beauverd@tx.group>
2024-08-07 01:14:13 +0200

    fix

--- lambda/ec2_simple/start/main.py
+++ lambda/ec2_simple/start/main.py
@@ -3,7 +3,6 @@ import boto3
 def lambda_handler(event, context):
     ec2 = boto3.client("ec2")
     instance_id = event.get("instance_id")
-
     try:
         response = ec2.start_instances(InstanceIds=[instance_id])
         print(response)

3ae49e5325a7fde44edfc91316f6a5a3ac79d8df
Davidoutz <david.beauverd@tx.group>
2024-08-22 09:30:46 +0200

    fix ec2 lambda

--- lambda/ec2_simple/stop/main.py
+++ lambda/ec2_simple/stop/main.py
@@ -1,19 +1,19 @@
 import boto3
 
 def lambda_handler(event, context):
-    instance_id = event['instance_id']
+    instance_ids = event["instance_ids"]
     try:
         ec2 = boto3.client('ec2')
-        response = ec2.stop_instances(InstanceIds=[instance_id])
+        response = ec2.stop_instances(InstanceIds=instance_ids)
         print(response)
         return {
             "statusCode": 200,
-            "body": f"Successfully stopped instance {instance_id}",
+            "body": f"Successfully stopped instance {instance_ids}",
             "response": response,
         }
     except Exception as e:
-        print(f"Error stopping instance {instance_id}: {str(e)}")
+        print(f"Error stopping instance {instance_ids}: {str(e)}")
         return {
             "statusCode": 500,
-            "body": f"Error stopping instance {instance_id}: {str(e)}",
+            "body": f"Error stopping instance {instance_ids}: {str(e)}",
         }
